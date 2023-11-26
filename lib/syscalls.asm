d654e50dcd26183fb56aef57b2066747abf4cc35
Edgar Luque <git@edgarluque.com>
2023-11-26 14:58:50 +0100

    ok

--- lib/syscalls.asm
+++ lib/syscalls.asm
@@ -1,22 +1,6 @@
 %ifndef INCLUDE_SYSCALLS
 %define INCLUDE_SYSCALLS
 
-struc SYSCALL_STAT
-    .st_dev: resd 1
-    .st_ino: resd 1
-    .st_mode: resw 1
-    .st_nlink: resw 1
-    .st_uid: resw 1
-    .st_gid: resw 1
-    .st_rdev: resd 1
-    .st_size: resd 1
-    .st_atime: resd 1
-    .st_mtime: resd 1
-    .st_ctime: resd 1
-    .st_blksize: resd 1
-    .st_blocks: resd 1
-endstruc
-
 SYS_EXIT    EQU 60
 SYS_OPEN    EQU 2
 SYS_READ    EQU 0
